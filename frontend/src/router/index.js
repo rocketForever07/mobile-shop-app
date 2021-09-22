4c85957d1cd1b3116a527c4b45514b9265e74119
caoduc354 <caoduc354@gmail.com>
2021-09-22 07:02:26 +0700

    add UI

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,8 +3,6 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import Shop from '@/components/Shop'
 import Product from '@/components/Product'
-import Blog from '@/components/Blog'
-import Post from '@/components/Post'
 import Cart from '@/components/Cart'
 import Layout from '@/components/Layout'
 
@@ -30,16 +28,6 @@ export default new Router({
           path:'/product',
           component:Product,
           name:'Product'
-        },
-        {
-          path:'/blog',
-          component:Blog,
-          name:'Blog'
-        },
-        {
-          path:'/post',
-          component:Post,
-          name:'Post'
         },
         {
           path:'/cart',
